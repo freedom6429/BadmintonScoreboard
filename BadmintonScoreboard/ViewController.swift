f7b588077dda751e2528fc15c164d8c98d5e8a87
Wen Lo <luowen0616@gmail.com>
2022-01-06 16:58:59 +0800

    Initial Commit

---
+++ BadmintonScoreboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BadmintonScoreboard
+//
+//  Created by Wen Luo on 2022/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
